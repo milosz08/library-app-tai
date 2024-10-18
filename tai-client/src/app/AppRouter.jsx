9fb06cbbd8c6c893f1b7b9f79779e75cf26d7f68
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-10-18 12:57:46 +0200

    add missing MainLayout root router element

--- tai-client/src/app/AppRouter.jsx
+++ tai-client/src/app/AppRouter.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from './MainLayout';
 
 const RootPage = React.lazy(() => import('./pages/root'));
 const NotFoundPage = React.lazy(() => import('./pages/not-found'));
 
 const router = createBrowserRouter([
-  { path: '/', element: <RootPage /> },
-  { path: '*', element: <NotFoundPage /> },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <RootPage /> },
+      { path: '*', element: <NotFoundPage /> },
+    ],
+  },
 ]);
 
 const AppRouter = () => {
