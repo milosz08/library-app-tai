98a2de6fde59068e0cbe1a1291c9e64fbc449b53
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-12-08 11:46:54 +0100

    create rental controller

---
+++ tai-server/src/main/java/pl/polsl/tai/network/rental/RentalController.java
@@ -0,0 +1,58 @@
+package pl.polsl.tai.network.rental;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import pl.polsl.tai.domain.book.BookEntity;
+import pl.polsl.tai.dto.PageableResDto;
+import pl.polsl.tai.network.rental.dto.LoanBookReqDto;
+import pl.polsl.tai.network.rental.dto.RentedBookDetailsResDto;
+import pl.polsl.tai.network.rental.dto.RentedBookRowResDto;
+import pl.polsl.tai.network.rental.dto.ReturnBookReqDto;
+import pl.polsl.tai.security.LoggedUser;
+
+@RestController
+@RequestMapping("/v1/rental")
+@RequiredArgsConstructor
+class RentalController {
+	private final RentalService rentalService;
+
+	@GetMapping("/rented")
+	ResponseEntity<PageableResDto<RentedBookRowResDto, BookEntity>> getPageableRentedBooks(
+		@RequestParam(required = false) String title,
+		@RequestParam(required = false) Integer page,
+		@RequestParam(required = false) Integer size,
+		@AuthenticationPrincipal LoggedUser loggedUser
+	) {
+		return ResponseEntity.ok(rentalService.getPageableRentedBooks(title, page, size, loggedUser));
+	}
+
+	@GetMapping("/rented/{bookId}/details")
+	ResponseEntity<RentedBookDetailsResDto> getRentedBookDetails(
+		@PathVariable Long bookId,
+		@AuthenticationPrincipal LoggedUser loggedUser
+	) {
+		return ResponseEntity.ok(rentalService.getRentedBookDetails(bookId, loggedUser));
+	}
+
+	@PatchMapping("/loan")
+	ResponseEntity<Void> loanBook(
+		@Valid @RequestBody LoanBookReqDto reqDto,
+		@AuthenticationPrincipal LoggedUser loggedUser
+	) {
+		rentalService.loanBook(reqDto, loggedUser);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("/return")
+	ResponseEntity<Void> returnBook(
+		@Valid @RequestBody ReturnBookReqDto reqDto,
+		@AuthenticationPrincipal LoggedUser loggedUser
+	) {
+		rentalService.returnBook(reqDto, loggedUser);
+		return ResponseEntity.noContent().build();
+	}
+}
+
---
+++ tai-server/src/main/java/pl/polsl/tai/network/rental/dto/ReturnBookReqDto.java
@@ -0,0 +1,17 @@
+package pl.polsl.tai.network.rental.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReturnBookReqDto {
+	@NotNull(message = "Identyfikator książki musi istnieć.")
+	private Long bookId;
+
+	@NotNull(message = "Ilość zwracanych książek musi istnieć.")
+	@Min(value = 1, message = "Ilość zwracanych książek musi być większa bądź równa 1.")
+	private Long count;
+}
