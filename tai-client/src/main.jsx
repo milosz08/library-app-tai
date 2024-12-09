6ec549ccf14c8913b73bd00a82564ed64c644257
Łukasz Krawczyk <101646747+trimplexx@users.noreply.github.com>
2024-12-09 08:07:54 +0100

    [FE] universal loader context.

--- tai-client/src/main.jsx
+++ tai-client/src/main.jsx
@@ -4,16 +4,19 @@ import { createRoot } from 'react-dom/client';
 import AppRouter from './app/AppRouter';
 import { AlertProvider } from './context/AlertContext';
 import { AuthProvider } from './context/AuthContext';
+import { LoaderProvider } from './context/LoaderContenxt';
 import theme from './utils/theme';
 
 createRoot(document.getElementById('app-mount')).render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <AuthProvider>
-        <AlertProvider>
-          <AppRouter />
-        </AlertProvider>
-      </AuthProvider>
+      <LoaderProvider>
+        <AuthProvider>
+          <AlertProvider>
+            <AppRouter />
+          </AlertProvider>
+        </AuthProvider>
+      </LoaderProvider>
     </React.StrictMode>
   </ThemeProvider>
 );
